d3d9fff1b63a4942d62d047585635a2bc3a50eca
Ajaya Ramachandran <ajaya@subconscious.dev>
2026-06-08 23:32:19 -0400

    added web ui

--- web/backend/server.py
+++ web/backend/server.py
@@ -24,7 +24,7 @@ app = FastAPI(title="MC Converter API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"],
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
